82bab72cf6dbb74d25b00fad28f097bbf4c281da
Tazri <mdtazri@gmail.com>
2022-08-31 23:25:54 +0600

    just learn how to setup camera in threejs

---
+++ day_1/js/script.js
@@ -0,0 +1,22 @@
+import * as THREE from 'three';
+import WebGl from 'three/examples/jsm/capabilities/WebGL';
+
+// check is webgl support or not
+if (WebGl.isWebGLAvailable()){
+    console.log("WEBGL ALL OK");
+}else{
+    console.log("There is no webgl");
+}
+
+// create renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// creating scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75, /** field of view */
+    window.innerWidth/window.innerHeight, /** aspect ratio */
+)
